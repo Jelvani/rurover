96fb92ea55a688e6195349f77ede586bd3df64d3
Jelvani <aj654@rutgers.edu>
2020-08-18 21:39:37 -0400

    alpha stage for manual control testing and data collection with ROS

--- mirocontroller/Teensy1/Teensy1.ino
+++ mirocontroller/Teensy1/Teensy1.ino
@@ -5,6 +5,7 @@
 #include <Encoder.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
+#include <Servo.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include "TeensyThreads.h"
@@ -13,9 +14,19 @@
 //ThreadWrap(Serial, SerialX);
 //#define Serial ThreadClone(SerialX)
 /**********Function Prototypes**************/
-
+void moveCb(const ackermann_msgs::AckermannDriveStamped &msg);
+void compute_steering_parameters();
+void vehicle_controller();
+void steering_read();
+void reciever_esc_read();
+void reciever_servo_read();
+void imu_read();
+float mapf(float x, float in_min, float in_max, float out_min, float out_max);
+void steering_isr();
+void rec_servo_isr();
+void rec_esc_isr();
  /***********************************/
- 
+ //TODO add service for switching to computer control
  /************VARIABLES********************/
 Encoder fl(5, 6);
 Encoder fr(7, 8);
@@ -27,13 +38,16 @@ Servo steer;
 ros::NodeHandle nh;
 std_msgs::Float32 flFeedback, frFeedback, rlFeedback, rrFeedback, steeringFeedback, manualSteering, manualThrottle;
 volatile sensor_msgs::Imu imu_dat;
-volatile unsigned long steering_pwm = 0; //0.08789 degrees per microsecond
+volatile unsigned long steering_pwm = 0; //0.351564 degrees per microsecond
 volatile unsigned long reciever_esc_pwm = 0;
 volatile unsigned long reciever_servo_pwm = 0;
-unsigned long MIN_pwm = 0;
+unsigned long MIN_pwm = 0; //range of PWM is about 112 microseconds
 unsigned long MAX_pwm = 0;
 unsigned long MIN_angle = 0;
 unsigned long MAX_angle = 0;
+volatile unsigned long esc_micros = 0;
+volatile unsigned long servo_micros = 0;
+volatile unsigned long steering_micros = 0;
 volatile float steering_angle = 0;
 
 bool manual_control = true;
@@ -54,17 +68,14 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55,0x28);
 volatile sensors_event_t orientationData , angVelocityData , linearAccelData;
 /************************************************/
 
-void moveCb(const ackermann_msgs::AckermannDriveStamped &msg){
-  if(!manual_control){
-    steering_angle = msg.drive.steering_angle
-}
-void setup()
-{
+
+void setup(){
   steer.attach(2);
   esc.attach(3);
-  pinMode(14,INPUT); //reciever esc pwm
-  pinMode(15,INPUT); //reciever servo pwm
-  pinMode(23,INPUT); //steering encoder pwm
+  
+  attachInterrupt(14,rec_esc_isr,CHANGE); //reciever esc pwm
+  attachInterrupt(15,rec_servo_isr,CHANGE); //reciever servo pwm
+  attachInterrupt(23,steering_isr,CHANGE); //steering encoder pwm
   if(!bno.begin())
   {
    //if not detected, infinite loop
@@ -89,21 +100,18 @@ void setup()
   /*************************/
   
   /****Add Threads****/
-  threads.addThread(steering_read);
-  threads.addThread(reciever_esc_read);
-  threads.addThread(reciever_servo_read);
   threads.addThread(imu_read);
-  threads.addThread(steering_angle_controller);
+  threads.addThread(vehicle_controller);
   /***********************/
 
 
 /*calibrate steering*/
   steer.write(0);
-  MIN_pwm = steering_pwm
+  MIN_pwm = steering_pwm;
   delay(1000);
   steer.write(0);
   delay(1000);
-  MAX_pwm = steering_pwm
+  MAX_pwm = steering_pwm;
   compute_steering_parameters();
 }
 
@@ -122,12 +130,23 @@ void loop()
   pub_rl_feedback.publish(&rlFeedback);
   rrFeedback.data = rr.read();
   pub_rr_feedback.publish(&rrFeedback);
-  steeringFeedback.data = map(steering_pwm, MIN_pwm, MAX_pwm, MIN_angle, MAX_angle);
+  steeringFeedback.data = mapf(steering_pwm, MIN_pwm, MAX_pwm, MIN_angle, MAX_angle);
   pub_steering_feedback.publish(&steeringFeedback);
-  manualSteering.data = reciever_servo_pwm
-  pub_manual_steering_command.pulish();
-  
-  imupub.publish(&imu_dat);//slows down serial, comment for debugging
+  manualSteering.data = reciever_servo_pwm;
+  pub_manual_steering_command.publish(&manualSteering);
+  manualThrottle.data = reciever_esc_pwm;
+  pub_manual_throttle_command.publish(&manualThrottle);
+  imu_dat.orientation.x = orientationData.orientation.x;
+  imu_dat.orientation.y = orientationData.orientation.y;
+  imu_dat.orientation.z = orientationData.orientation.z;
+  imu_dat.angular_velocity.x = angVelocityData.gyro.x;
+  imu_dat.angular_velocity.y = angVelocityData.gyro.y;
+  imu_dat.angular_velocity.z = angVelocityData.gyro.z;
+  imu_dat.linear_acceleration.x = linearAccelData.acceleration.x;
+  imu_dat.linear_acceleration.y = linearAccelData.acceleration.y;
+  imu_dat.linear_acceleration.z = linearAccelData.acceleration.z;
+  imu_dat.header.stamp = nh.now();
+  pub_imu.publish(&imu_dat);//slows down serial, comment for debugging
   nh.spinOnce();
   currentMillis=millis();
   currentMillis=currentMillis-startMillis;
@@ -137,68 +156,31 @@ void loop()
   
 }
 
+void moveCb(const ackermann_msgs::AckermannDriveStamped &msg){
+  if(!manual_control){
+    steering_angle = msg.drive.steering_angle;
+    }
+}
+
 void compute_steering_parameters(){
-  float range = abs(MAX_pwm - MIN_pwm)*0.08789;
+  float range = abs(MAX_pwm - MIN_pwm)*0.351564;
   range = range/2;
   MIN_angle = -1*range;
   MAX_angle = range;
 }
-void steering_controller(){
+void vehicle_controller(){
   while(1){
   
     if(manual_control){
       steer.writeMicroseconds(reciever_servo_pwm);
+      esc.writeMicroseconds(reciever_esc_pwm);
     }else{
       
     }
     threads.delay(10); //100 hz loop
   }
-  
-}
-void steering_read(){
-  while(1){
-  steering_pwm = pulseIn(23, HIGH); //length of pwm pulse
-  threads.delay(5); //200 hz loop
-  }
 }
 
-void reciever_esc_read(){
-  while(1){
-  reciever_esc_pwm = pulseIn(15, HIGH); //length of pwm pulse
-  threads.delay(10); //100 hz loop
-  }
-}
-
-void reciever_servo_read(){
-  while(1){
-  reciever_servo_pwm = pulseIn(14, HIGH); //length of pwm pulse
-  threads.delay(10); //100 hz loop
-  }
-}
-
-void steering_angle_controller(){//ISR for front steering servo
-  while(1){
-    /*
-  if(abs(myEnc.read())>1400){ //for max and min turning angle
-    analogWrite(11, 0);
-    return;
-  }*/
-    frErr=frSp-front.read();
-    if(frErr>=0){
-      digitalWrite(1, LOW);
-    }else{
-      digitalWrite(1, HIGH);
-      frErr=frErr*(-1);
-    }
-    if(frErr*100>60000){
-      analogWriteFrequency(0, 60000);
-    }else{
-      analogWriteFrequency(0, frErr*100);
-    }
-    analogWrite(0, 200);
-    threads.delay(5); 
-  }
-}
 
 
 void imu_read(){
@@ -206,16 +188,46 @@ void imu_read(){
     bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
     bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
     bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-    imu_dat.orientation.x = orientationData.orientation.x;
-    imu_dat.orientation.y = orientationData.orientation.y;
-    imu_dat.orientation.z = orientationData.orientation.z;
-    imu_dat.angular_velocity.x = angVelocityData.gyro.x;
-    imu_dat.angular_velocity.y = angVelocityData.gyro.y;
-    imu_dat.angular_velocity.z = angVelocityData.gyro.z;
-    imu_dat.linear_acceleration.x = linearAccelData.acceleration.x;
-    imu_dat.linear_acceleration.y = linearAccelData.acceleration.y;
-    imu_dat.linear_acceleration.z = linearAccelData.acceleration.z;
-    imu_dat.header.stamp = nh.now();
     threads.delay(10); //100 hz loop
   }
+}
+void rec_esc_isr(){
+  if(digitalRead(14) == HIGH){
+    esc_micros = micros();
+  }else{
+    if(esc_micros > 0){
+      if(micros() > esc_micros){
+        reciever_esc_pwm = micros() - esc_micros;
+      }
+    }
+  }
+}
+
+void rec_servo_isr(){
+  if(digitalRead(15) == HIGH){
+    servo_micros = micros();
+  }else{
+    if(servo_micros > 0){
+      if(micros() > servo_micros){
+        reciever_servo_pwm = micros() - servo_micros;
+      }
+    }
+  }
+}
+
+void steering_isr(){
+  if(digitalRead(23) == HIGH){
+    steering_micros = micros();
+  }else{
+    if(steering_micros > 0){
+      if(micros() > steering_micros){
+        steering_pwm = micros() - steering_micros;
+      }
+    }
+  }
+}
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
